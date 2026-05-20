1ab744bc59da76efaa8fe08ee1f9ae1b31e1dc75
Abirami <Abirami.P@Solidpro-es.com>
2026-05-20 18:18:43 +0530

    alembic and env.py changed

--- frontend-erp/app/features/item-master/page.tsx
+++ frontend-erp/app/features/item-master/page.tsx
@@ -1,5 +1,4 @@
-
-import {ItemPage} from "./item-page";
+import { ItemPage } from "./item-page";
 import { getItems } from "@/lib/api/items";
 export default async function Page() {
   const data = await getItems();
